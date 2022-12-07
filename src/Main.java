63bb7678bad5c9bd6d1009e1514e9201dc10256c
alejo-quin <alejandrodonquin@gmail.com>
2022-12-07 17:06:54 -0500

    Tercer ejercicio del curso de open bootcamp

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+//Crear clase Persona.
+//Crear variables las privadas edad, nombre y telefono de la clase Persona.
+//Crear gets y sets de cada propiedad.
+//Crear un objeto persona en el main.
+//Utiliza los gets y sets para darle valores a las propiedades edad,
+// nombre y telefono, por último muéstralas por consola
+public class Main {
+    public static void main(String[] args){
+        Persona persona = new Persona();
+        persona.setEdad(33);
+        persona.setNombre("Alejandro");
+        persona.setTelefono("3142113285");
+
+        System.out.println( persona.getEdad());
+        System.out.println( persona.getNombre());
+        System.out.println( persona.getTelefono());
+
+    }
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
